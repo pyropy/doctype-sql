9cb249a3f4e583e377f55bf0153cf033d84eb634
Srdjan S <stankovic.srdjo@gmail.com>
2020-02-06 10:18:08 +0100

    Select query db and reformat queries

--- queries.sql
+++ queries.sql
@@ -1,15 +1,17 @@
+USE twitter;
+
 -- select last 30 tweets where username is Mark
 SELECT *
-FROM   twitter.tweets,
-       twitter.users
-WHERE  twitter.tweets.user = "Mark"
-ORDER  BY twitter.tweets.id DESC
+FROM   tweets,
+       users
+WHERE  tweets.user = "Mark"
+ORDER  BY tweets.id DESC
 LIMIT  30;
 
 -- offset by 30 tweets and select next 30 tweets for user with username Mark
 SELECT *
-FROM   twitter.tweets,
-       twitter.users
-WHERE  twitter.tweets.user = "Mark"
-ORDER  BY twitter.tweets.id DESC
+FROM   tweets,
+       users
+WHERE  tweets.user = "Mark"
+ORDER  BY tweets.id DESC
 LIMIT  30, 30;  
