cd0c75fb2c5c2f124175198d89a7150c6794d308
Srdjan S <stankovic.srdjo@gmail.com>
2020-02-06 10:21:11 +0100

    Use explicit join

--- queries.sql
+++ queries.sql
@@ -2,16 +2,18 @@ USE twitter;
 
 -- select last 30 tweets where username is Mark
 SELECT *
-FROM   tweets,
-       users
-WHERE  tweets.user = "Mark"
+FROM   tweets
+       JOIN users
+         ON users.username = tweets.user
+WHERE  tweets.user = "mark"
 ORDER  BY tweets.id DESC
 LIMIT  30;
 
 -- offset by 30 tweets and select next 30 tweets for user with username Mark
 SELECT *
-FROM   tweets,
-       users
-WHERE  tweets.user = "Mark"
+FROM   tweets
+       JOIN users
+         ON users.username = tweets.user
+WHERE  tweets.user = "mark"
 ORDER  BY tweets.id DESC
 LIMIT  30, 30;  
