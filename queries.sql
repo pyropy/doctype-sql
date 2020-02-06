ecc1ca8cb6e38d40bdf45d665c52b3e321b8932d
Srdjan S <stankovic.srdjo@gmail.com>
2020-02-06 10:23:43 +0100

    Add aliases to queries

--- queries.sql
+++ queries.sql
@@ -2,18 +2,18 @@ USE twitter;
 
 -- select last 30 tweets where username is Mark
 SELECT *
-FROM   tweets
-       JOIN users
-         ON users.username = tweets.user
-WHERE  tweets.user = "mark"
-ORDER  BY tweets.id DESC
+FROM   tweets T
+       JOIN users U
+         ON U.username = T.user
+WHERE  T.user = "mark"
+ORDER  BY T.id DESC
 LIMIT  30;
 
 -- offset by 30 tweets and select next 30 tweets for user with username Mark
 SELECT *
-FROM   tweets
-       JOIN users
-         ON users.username = tweets.user
-WHERE  tweets.user = "mark"
-ORDER  BY tweets.id DESC
+FROM   tweets T
+       JOIN users U
+         ON U.username = T.user
+WHERE  T.user = "mark"
+ORDER  BY T.id DESC
 LIMIT  30, 30;  
