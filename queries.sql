409a8f90bbcc5db390c7199f79f2c7a2685d5445
Srdjan S <stankovic.srdjo@gmail.com>
2020-02-08 16:20:54 +0100

    Edit queries and table schema
    Add surogat keys for users
    Update queries to return tweets of users followed by user "Mark"

--- queries.sql
+++ queries.sql
@@ -1,19 +1,36 @@
 USE twitter;
 
 -- select last 30 tweets where username is Mark
-SELECT *
-FROM   tweets T
-       JOIN users U
-         ON U.username = T.user
-WHERE  T.user = "Mark"
+SELECT T.*,
+       FU.username,
+       FU.first_name,
+       FU.last_name
+FROM   users U
+       INNER JOIN followers F
+               ON ( F.follower = U.id )
+       INNER JOIN users FU
+               ON ( FU.id = F.following )
+       INNER JOIN tweets T
+               ON( T.user = F.id )
+WHERE  U.username = "mark"
+ORDER  BY T.id DESC
+LIMIT  30;  
+
+-- lets say we have 60 tweets in our db, we can send 
+-- id of last tweet and say that all other id's must be 
+-- less then the last id (e.g. id = 30)
+SELECT T.*,
+       FU.username,
+       FU.first_name,
+       FU.last_name
+FROM   users U
+       INNER JOIN followers F
+               ON ( F.follower = U.id )
+       INNER JOIN users FU
+               ON ( FU.id = F.following )
+       INNER JOIN tweets T
+               ON( T.user = F.id )
+WHERE  U.username = "mark"
+       AND T.id < 30 -- e.g. 30 is last post id that we've loaded
 ORDER  BY T.id DESC
 LIMIT  30;
-
--- offset by 30 tweets and select next 30 tweets for user with username Mark
-SELECT *
-FROM   tweets T
-       JOIN users U
-         ON U.username = T.user
-WHERE  T.user = "Mark"
-ORDER  BY T.id DESC
-LIMIT  30, 30;  
--- twitter_mysql_create.sql
+++ twitter_mysql_create.sql
@@ -1,19 +1,19 @@
 -- Would use SHA-256 for encypting passwords
 -- Encrypted passwords can fit in 64 CHAR 
 CREATE TABLE `users` (
-	`username` char(64) NOT NULL,
-	`first_name` char(64) NOT NULL,
-	`last_name` char(64) NOT NULL,
-	`password` char(64) NOT NULL,
+	`username` VARCHAR(64) NOT NULL,
+	`first_name` VARCHAR(64) NOT NULL,
+	`last_name` VARCHAR(64) NOT NULL,
+	`password` VARCHAR(64) NOT NULL,
 	`created` TIMESTAMP NOT NULL,
-	`email` char(128) NOT NULL UNIQUE,
+	`email` VARCHAR(255) NOT NULL UNIQUE,
 	PRIMARY KEY (`username`)
 );
 
 CREATE TABLE `followers` (
 	`id` bigint NOT NULL AUTO_INCREMENT,
-	`follower` char(64) NOT NULL,
-	`following` char(64) NOT NULL,
+	`follower` VARCHAR(64) NOT NULL,
+	`following` VARCHAR(64) NOT NULL,
 	`created` TIMESTAMP NOT NULL,
 	PRIMARY KEY (`id`)
 );
@@ -22,7 +22,7 @@ CREATE TABLE `followers` (
 -- maximum tweet length
 CREATE TABLE `tweets` (
 	`id` bigint NOT NULL AUTO_INCREMENT,
-	`user` char(64) NOT NULL,
+	`user` VARCHAR(64) NOT NULL,
 	`content` TEXT(280) NOT NULL,
 	`created` TIMESTAMP NOT NULL,
 	PRIMARY KEY (`id`)
